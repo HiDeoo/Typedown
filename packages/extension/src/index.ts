1e00fcfabfaa2cb6ca483c395eefae1197c6dda5
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-09-06 09:04:58 +0200

    Refactor webview creation

--- packages/extension/src/index.ts
+++ packages/extension/src/index.ts
@@ -1,4 +1,6 @@
-import { commands, ExtensionContext, Uri, ViewColumn, window } from 'vscode'
+import { commands, ExtensionContext, window } from 'vscode'
+
+import { createWebviewPanel } from './webview'
 
 export function activate(context: ExtensionContext): void {
   context.subscriptions.push(commands.registerCommand('typedown.tsToMd', () => tsToMd(context)))
@@ -7,22 +9,5 @@ export function activate(context: ExtensionContext): void {
 function tsToMd(context: ExtensionContext) {
   window.showInformationMessage('Hello World from Typedown!')
 
-  const panel = window.createWebviewPanel('typedown', 'Typedown', ViewColumn.Active, { enableScripts: true })
-
-  const jsURI = panel.webview.asWebviewUri(Uri.joinPath(context.extensionUri, 'dist/webview/assets', 'index.js'))
-  const cssURI = panel.webview.asWebviewUri(Uri.joinPath(context.extensionUri, 'dist/webview/assets', 'index.css'))
-
-  panel.webview.html = `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Typedown</title>
-    <script type="module" crossorigin src="${jsURI}"></script>
-    <link rel="stylesheet" href="${cssURI}">
-  </head>
-  <body>
-    <div id="app"></div>
-  </body>
-</html>`
+  createWebviewPanel(context)
 }
