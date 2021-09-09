4d284a521f2ced7e74175184cf8e3959bd67187f
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-09-09 07:59:08 +0200

    Clean definition styles

--- packages/typedown-webview/src/components/Definition.svelte
+++ packages/typedown-webview/src/components/Definition.svelte
@@ -12,14 +12,37 @@
   }
 </script>
 
-<strong>{identifier}</strong>
-<Checkbox checked={definition.selected === true} on:change={onSelectDefinition}
-  >{JSON.stringify(definition)} - {JSON.stringify(definition)} - {JSON.stringify(definition)}</Checkbox
->
-<hr />
+<div class="definition">
+  <div class="definitionMask" />
+  <strong>{identifier}</strong>
+  <Checkbox checked={definition.selected === true} on:change={onSelectDefinition}
+    >{JSON.stringify(definition)} - {JSON.stringify(definition)} - {JSON.stringify(definition)}</Checkbox
+  >
+</div>
 
 <style>
+  .definition {
+    padding: 12px 14px 26px 14px;
+    position: relative;
+  }
+
+  .definitionMask {
+    background-color: var(--vscode-textCodeBlock-background);
+    bottom: 0;
+    display: none;
+    left: 0;
+    opacity: 0.6;
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+
+  .definition:hover .definitionMask {
+    display: block;
+  }
+
   strong {
     font-size: 1.1em;
+    position: relative;
   }
 </style>
