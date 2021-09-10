396b05fc52decb3dc3f664dfb5bdee3bd10f10b9
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-09-10 09:55:46 +0200

    Remove persistent writable rollup warning

--- packages/typedown-webview/src/lib/persistentWritable.ts
+++ packages/typedown-webview/src/lib/persistentWritable.ts
@@ -1,4 +1,5 @@
-import { Writable, writable } from 'svelte/store'
+import type { Writable } from 'svelte/store'
+import { writable } from 'svelte/store'
 
 export function persistentWritable<T>(key: string, initialValue: T): PersistentWritable<T> {
   const store = writable<T>(initialValue)
