0fe9f14443fc9d2396262765f30f69511cf36176
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-10-07 15:59:50 +0200

    Update type table type UI

---
+++ packages/typedown-webview/src/components/Code.svelte
@@ -0,0 +1,12 @@
+<code>
+  <slot />
+</code>
+
+<style>
+  code {
+    background-color: var(--vscode-checkbox-background);
+    border-radius: 3px;
+    font-size: 0.95em;
+    padding: 0.1em 0.3em;
+  }
+</style>
--- packages/typedown-webview/src/components/DefinitionTypeAlias.svelte
+++ packages/typedown-webview/src/components/DefinitionTypeAlias.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import type { TypeAliasDefinition } from 'typedown-shared'
 
+  import Code from './Code.svelte'
+
   export let definition: TypeAliasDefinition
 </script>
 
 <tbody>
   <tr>
     <td>{definition.description}</td>
-    <td>{definition.type}</td>
+    <td><Code>{definition.type}</Code></td>
   </tr>
 </tbody>
