299b090ff39786b42f822a5cb7f5d55eaa491cc8
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2022-03-20 13:01:14 +0100

    Remove console.error test stubs

--- packages/typedown-extension/src/tests/no-tsconfig/errors.test.ts
+++ packages/typedown-extension/src/tests/no-tsconfig/errors.test.ts
@@ -4,33 +4,23 @@ import { Uri, window } from 'vscode'
 
 import { fileToMd, folderToMd, getDefinitionsFromFixture } from '../utils'
 
-let consoleErrorStub: sinon.SinonStub<Parameters<typeof console.error>>
 let windowShowErrorMessageStub: sinon.SinonStub<Parameters<typeof window.showErrorMessage>>
 
 function assertNoTSConfigErrors() {
-  const errorStr = 'Could not find a TSConfig file for your workspace.'
-
-  assert(consoleErrorStub.calledOnce)
-
-  const error = consoleErrorStub.args[0]?.[0]
-
-  if (!error || !(error instanceof Error)) {
-    throw new Error(`console.error() expected to be called with an error but called instead with: ${typeof error}.`)
-  }
-
-  assert.equal(errorStr, error.message)
-
-  assert(windowShowErrorMessageStub.calledOnceWith(errorStr, { detail: undefined, modal: true }))
+  assert(
+    windowShowErrorMessageStub.calledOnceWith('Could not find a TSConfig file for your workspace.', {
+      detail: undefined,
+      modal: true,
+    })
+  )
 }
 
 describe('no ts-config', () => {
   beforeEach(() => {
-    consoleErrorStub = sinon.stub(console, 'error')
     windowShowErrorMessageStub = sinon.stub(window, 'showErrorMessage')
   })
 
   afterEach(() => {
-    consoleErrorStub.restore()
     windowShowErrorMessageStub.restore()
   })
 
